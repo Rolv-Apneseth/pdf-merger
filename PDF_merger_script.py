ec309de2044565269c809134984d5f8e33945fbb
Rolv-Apneseth <rolv.apneseth@gmail.com>
2020-08-17 18:22:26 +0100

    Added a main function
    Added a main function and a if __name__ block

--- PDF_merger_script.py
+++ PDF_merger_script.py
@@ -11,18 +11,24 @@ def merge(pdfs):
         merger.append(pdf)
     merger.write("Merged.pdf")
 
+def main(inputs):
 
-if str(sys.argv[1]) == "all":
-    pdfs = glob.iglob("*.pdf")
-else:
-    pdfs = sys.argv[1:]
-    for pdf in pdfs:
-        if not pdf.endswith(".pdf"):
-            pdfs.remove(pdf)
+    if str(inputs[1]) == "all":
+        pdfs = glob.iglob("*.pdf")
+    else:
+        pdfs = inputs[1:]
+        for pdf in pdfs:
+            if not pdf.endswith(".pdf"):
+                pdfs.remove(pdf)
 
-try:
-    merge(pdfs)
-except:
-    print("There was a problem merging the files. Make sure all files specified are .pdf files and they all exist in the same directory as the PDF_merger_script.py")
+    try:
+        if len(pdfs) > 1:
+            merge(pdfs)
+        else:
+            print("There were not 2 or more valid pdf files specified.")
+    except:
+        print("There was a problem merging the files. Make sure all files specified are .pdf files and they all exist in the same directory as the PDF_merger_script.py")
 
 
+if __name__ == "__main__":
+    main(inputs)
